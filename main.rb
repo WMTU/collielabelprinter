4f9c477da82a764851b31c8acc0981d48fbae825
Neil Betham <nbetham@me.com>
2014-10-19 02:10:21 -0400

    Update to use yam for config stuff

--- main.rb
+++ main.rb
@@ -2,6 +2,15 @@
 # encoding: utf-8
 require "rubygems"
 require "bunny"
+require "logger"
+require "yaml"
+
+# Load configuration settings
+CONFIG = YAML.load_file('config.yml')
+
+# Setup logger
+logger = Logger.new(STDOUT)
+logger.level = Logger::WARN
 
 # Consumer Class for Print Jobs
 class ZebraJobConsumer < Bunny::Consumer
@@ -14,10 +23,8 @@ class ZebraJobConsumer < Bunny::Consumer
   end
 end
 
-
-PRINTER_NAME = "Zebra_Technologies_ZTC_GK420d"
-
-mq = Bunny.new "amqp://collie:1234@chet.wmtu.local:5672"
+# Setup RabbitMQ connection
+mq = Bunny.new "amqp://#{CONFIG["RABBITMQ"]["USER"]}:#{CONFIG["RABBITMQ"]["PASS"]}@#{CONFIG["RABBITMQ"]["HOST"]}:#{CONFIG["RABBITMQ"]["PORT"]}"
 mq.start
 
 channel = mq.create_channel
@@ -27,16 +34,16 @@ queue = channel.queue("print_queue").bind(exchange, :routing_key => "print_queue
 print_queue_consumer = ZebraJobConsumer.new(channel, queue)
 
 print_queue_consumer.on_delivery do |delivery_info, metadata, payload|
-  puts "Got print job"
+  logger.info "Got print job"
 
   # Build command to print label
-  command = %W(lpr -P #{PRINTER_NAME} -o raw)
+  command = %W(lpr -P #{CONFIG["CUPS_PRINTER_NAME"]} -o raw)
 
   process = IO.popen(command, "r+")
   process.write(payload)
   process.close_write()
   Process.wait(process.pid)
-  puts "Print queued"
+  logger.info "Print queued"
 end
 
 queue.subscribe_with(print_queue_consumer, :block => true)
